ef64255a5f7dd87445b7d11430f623f987f9a964
Ben Williams <codebencode@icloud.com>
2024-12-05 13:59:17 +0000

    Add startQuiz & showQuestions functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 const questions = [
-    {
+    { // question 1
         question: 'Which is the largest animal in the world',
         answers: [
             { text: 'Shark', correct: false},
@@ -8,7 +8,7 @@ const questions = [
             { text: 'Giraffe', correct: false},
         ]
     },
-    
+    // question 2
     { question: 'Which is the largest desert in the world',
         answers: [
             { text: 'Gobi', correct: false},
@@ -17,7 +17,7 @@ const questions = [
             { text: 'Kalahari', correct: false},
         ]
     },
-
+// question 3
     {
         question: 'Which is the smallest continent in the world',
         answers: [
@@ -34,3 +34,29 @@ const questionElement = document.getElementById('question');
 const answerButtons = document.getElementById('answer-buttons');
 const nextButton = document.getElementById('next-btn');
 const backButton = document.getElementById('back-btn');
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() { // when quiz is stated it resets to 0 
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = 'Next'; 
+    showQuestion();
+}
+
+function showQuestion() {
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex +1;
+    questionElement.innerHTML = questionNo + '. ' + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerHTML = answer.text;
+        button.classList.add('btn');
+        answerButtons.appendChild(button);
+    })
+}
+
+startQuiz();
+
