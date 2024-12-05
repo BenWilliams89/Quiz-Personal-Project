9ef373e0d17b634f92e0f71d7a440bbb4b2cddaa
Ben Williams <codebencode@icloud.com>
2024-12-05 10:58:18 +0000

    Add varibales for our IDs

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,3 +29,8 @@ const questions = [
     }
 
 ]
+
+const questionElement = document.getElementById('question');
+const answerButtons = document.getElementById('answer-buttons');
+const nextButton = document.getElementById('next-btn');
+const backButton = document.getElementById('back-btn');
